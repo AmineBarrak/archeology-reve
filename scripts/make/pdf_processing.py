42c3b6ffc1d06fc2673e2d0e328d17fae6642bb8
amine <aminebarrak@live.com>
2021-11-05 08:43:37 -0400

    pdf processing update

--- scripts/make/pdf_processing.py
+++ scripts/make/pdf_processing.py
@@ -22,6 +22,33 @@ import spacy
 import regex as re
 
 
+def images_extraction(pdf_path):
+	# pip install pymupdf
+	# open the file
+	pdf_file = fitz.open(pdf_path)
+	  
+	# STEP 3
+	# iterate over PDF pages
+	for page_index in range(len(pdf_file)):
+	    
+	    # get the page itself
+	    page = pdf_file[page_index]
+	   
+	    for image_index, img in enumerate(page.getImageList(), start=1):
+	        
+	        # get the XREF of the image
+	        xref = img[0]
+	          
+	        # extract the image bytes
+	        base_image = pdf_file.extractImage(xref)
+	        # get the image extension
+	        image_ext = base_image["ext"]
+	        
+	        
+	        image_bytes = base_image["image"]
+	        image = Image.open(io.BytesIO(image_bytes))
+	        image.save(str(page_index)+"_"+str(image_index)+"."+image_ext)
+
 
 def fix_rotated_text(text):
 	
